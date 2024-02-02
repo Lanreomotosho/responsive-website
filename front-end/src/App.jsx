a4ba318e4ebc053e7a1a926d2f25a28e387af759
Lanre Omotosho <omotoshoolanrewaju@gmail.com>
2024-02-02 09:03:22 +0100

    first commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Header, Info, Topics, Blog, Testimonials, Footer } from './components';
+
 
 const App = () => {
   return (
-    <div>Hello</div>
+  <>
+  <Header />
+  <Topics />
+  <Info />
+  <Blog />
+  <Testimonials />
+  <Footer />
+  </>
   )
-}
+
+
+};
 
 export default App
